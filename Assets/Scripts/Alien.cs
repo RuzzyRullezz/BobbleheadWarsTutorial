a3e4feb37294399cdf9a53714e978f583eb298e9
RuzzyRullezz <ruslan@lemimi.ru>
2020-07-03 17:42:39 +0300

    Chapter 5

---
+++ Assets/Scripts/Alien.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Alien : MonoBehaviour {
+
+    public Transform target;
+    public float navigationUpdate;
+    private float navigationTime = 0;
+    private NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+        navigationTime += Time.deltaTime;
+        if (navigationTime > navigationUpdate)
+        {
+            agent.destination = target.position;
+            navigationTime = 0;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
